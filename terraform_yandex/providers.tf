2c2875fb8a02808e9db1a45ca8aa8fee9a3e593e
Назаров Антон <antowko.an@gmail.com>
2024-10-11 19:05:04 +0700

    ver_1.2
    ver_1.2

---
+++ terraform_yandex/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+  cloud_id  = "<cloud_id>"
+  folder_id = "<folder_id>"
+  token     = "<token>"
+}
